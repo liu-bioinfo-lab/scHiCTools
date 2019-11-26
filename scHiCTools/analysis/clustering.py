ed36ba7932a6632beca78e8530a80f52aef91103
xinjunUM <lixinjun@umich.edu>
2019-11-26 10:26:43 -0500

    add unnormalized spectral clustering in clustering.py.

--- scHiCTools/analysis/clustering.py
+++ scHiCTools/analysis/clustering.py
@@ -9,6 +9,7 @@
 """
 
 import numpy as np
+from scipy.sparse import csgraph
 
 
 def kmeans(data, k=4,
@@ -58,3 +59,58 @@ def kmeans(data, k=4,
                 centroids[j]=np.average(group, axis=0)
 
     return(label)
+
+
+
+
+# spectral_clustering can directly takes similarity matrix.
+def spectral_clustering(data,
+                        data_type='graph',
+                        n_clusters=4,
+                        method='Unnormalized'):
+
+    """
+    This function is a impliment of unnormalized spectral clustering.
+
+    Input:
+        data: a numpy array, can either be a matrix which every row represent a point
+            or a graph similarity matrix.
+        data_type: specify the type of data input.
+        k: number of clusters.
+
+    Output:
+        a list of the label of the data points.
+    """    
+
+    # compute graph similarity matrix
+    if method=='Unnormalized':
+        if data_type=='graph' and len(data)==len(data[0]):
+            if len(data)!=len(data[0]):
+                raise ValueError('data is not a distance matrix!')
+            elif np.sum(data.T!=data)>0:
+                raise ValueError('data is not a distance matrix!')
+            elif sum(np.diag(data)!=0)!=0:
+                raise ValueError('data is not a distance matrix!')
+            graph=data
+        elif data_type=='points':
+            # Use pairwised Euclidean distances to compute graph
+            graph = np.sum(np.square(data), 1)
+            graph = np.add(np.add(-2 * np.dot(data, data.T), graph).T, graph)
+        else:
+            raise ValueError('data_type is not supported.')
+
+        # the unnormalized Laplacian L.
+        L=csgraph.laplacian(graph, normed=False)
+
+        # eigenvalues and eigenvectors.
+        eig_vals, eig_vecs = np.linalg.eig(L)
+        eig_vecs= eig_vecs[:, np.flipud(eig_vals.argsort())]
+        eig_vals= np.flipud(np.sort(eig_vals))
+
+        # first k=n_clusters eigenvectors.
+        U=eig_vecs[:,range(n_clusters)]
+
+        # Clustering using k-means
+        label=kmeans(U,k=n_clusters)
+
+        return(label)
