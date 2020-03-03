ac987ee79be7a8f8f3c1380b40a6870facbdc235
xinjunUM <lixinjun@umich.edu>
2020-03-03 17:39:54 -0500

    Create example2.py

---
+++ example/example2.py
@@ -0,0 +1,46 @@
+import sys
+sys.path.insert(0, './')
+print(sys.path)
+import numpy as np
+from scHiCTools import scHiCs
+from scHiCTools import scatter
+import matplotlib.pyplot as plt
+import os
+
+
+os.chdir("D:/学习/密歇根/Liu LAB/scHiCTools/example") # set your current directory
+files=os.listdir(os.getcwd()+'/1CDX_cells')
+for i in range(len(files)):
+    files[i]='1CDX_cells/'+files[i]+'/new_adj'
+files=np.random.choice(files, size=20, replace=False)
+
+
+x = scHiCs(files, reference_genome='mm9', resolution=500000, max_distance=4000000,
+            format='shortest_score', resolution_adjust=True, chromosomes='except Y',
+            operations=['convolution'], kernel_shape=3, keep_n_strata=10, store_full_map=True)
+
+
+
+emb=x.learn_embedding('inner_product', 'MDS', dim=2, n_strata=10)
+hicluster=x.scHiCluster()
+label=x.clustering(n_clusters=4, clustering_method='kmeans', similarity_method='innerproduct', aggregation='median')
+
+print(emb)
+print(hicluster)
+print(label)
+
+
+fig=plt.figure()
+plt.subplot(121)
+
+ax1=plt.subplot(1, 2, 1)
+scatter(emb,label=label,point_size=5)
+ax1.set_title('InnerProduct + MDS + Kmeans embedding')
+
+# add a subplot with no frame
+ax2=plt.subplot(1, 2, 2)
+scatter(hicluster[0],label=hicluster[1],point_size=5)
+ax2.set_title('scHiCluster')
+
+plt.show()
+
