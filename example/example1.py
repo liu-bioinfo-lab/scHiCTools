5cce63c80f8ab59f2b2664ea4b27fd48e8925a36
xinjunUM <lixinjun@umich.edu>
2020-03-03 17:39:48 -0500

    Create example1.py

---
+++ example/example1.py
@@ -0,0 +1,25 @@
+from time import time
+import sys
+sys.path.insert(0, '../')
+print(sys.path)
+from scHiCTools import scHiCs
+
+
+# Learn the embeddings of cell_01, cell_02 and cell_03
+# Time:
+# Time 0: input I/O and pre-processing
+# Time 1: process each strata
+# Time 2: calculating pairwise similarity
+
+t0 = time()
+x = scHiCs(['cell_01', 'cell_02', 'cell_03'], reference_genome='mm9', resolution=500000, max_distance=4000000,
+           format='shortest_score', resolution_adjust=False, chromosomes='except Y',
+           operations=['convolution'], kernel_shape=3, keep_n_strata=10, store_full_map=False
+           )
+print('Time 0:', time() - t0)
+emb, _ = x.learn_embedding(similarity_method='selfish', embedding_method='MDS', aggregation='median', print_time=False, return_distance=True)
+print(emb)
+print(_)
+
+
+
