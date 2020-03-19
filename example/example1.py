841fe89e837bb929b936d84d4148777f7bb4c770
xinjunUM <lixinjun@umich.edu>
2020-03-18 21:50:15 -0400

    Update example1.py

--- example/example1.py
+++ example/example1.py
@@ -1,26 +1,24 @@
-from time import time
+
 import sys
-sys.path.insert(0, '../')
+import os
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+
 print(sys.path)
 from scHiCTools import scHiCs
 
+x = scHiCs(['cell_01','cell_02','cell_03'], reference_genome='mm9', resolution=500000, max_distance=4000000,
+           format='shortest_score', resolution_adjust=True, chromosomes='except Y',
+           operations=['convolution'], kernel_shape=3, keep_n_strata=10, store_full_map=False
+           )
 
-# Learn the embeddings of cell_01, cell_02 and cell_03
-# Time:
-# Time 0: input I/O and pre-processing
-# Time 1: process each strata
-# Time 2: calculating pairwise similarity
-
-t0 = time()
-x = scHiCs(['4DNFIW961HN2.mcool'], reference_genome='hg38', resolution=500000, max_distance=4000000,
+x = scHiCs(, reference_genome='hg38', resolution=500000, max_distance=4000000,
            format='mcool', resolution_adjust=False, chromosomes='except Y',
            operations=['convolution'], kernel_shape=3, keep_n_strata=10, store_full_map=False
            )
-# print('Time 0:', time() - t0)
-# emb, _ = x.learn_embedding(similarity_method='selfish', embedding_method='MDS', aggregation='median', print_time=False, return_distance=True)
-# print(emb)
-# print(_)
 
-print(x)
+emb, _ = x.learn_embedding(similarity_method='innerproduct', embedding_method='MDS', aggregation='median', print_time=False, return_distance=True)
 
+print(emb)
+print(_)
 
