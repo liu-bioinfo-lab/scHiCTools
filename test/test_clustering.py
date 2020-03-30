5d94969563a4d12f0cf38e1be0ae405fbbc3a4c8
xinjunUM <lixinjun@umich.edu>
2020-03-30 15:12:00 -0400

    Update test_clustering.py

--- test/test_clustering.py
+++ test/test_clustering.py
@@ -12,14 +12,14 @@ sys.path.insert(0, os.path.abspath(
 from scHiCTools import kmeans, spectral_clustering, HAC
 
 
-center=np.array([[0,0],[20,20],[-20,20]])
-rand_data = np.random.normal(size=(30,2)) + np.repeat(center, 10, axis=0)
+center=np.array([[0,0],[100,100],[-100,100]])
+rand_data = np.random.normal(size=(90,2)) + np.repeat(center, 30, axis=0)
 
 def test_kmeans():
     label=kmeans(rand_data, k=3)
-    l1 = np.equal(label[:10], label[0]).all()
-    l2 = np.equal(label[10:-10], label[10]).all()
-    l3 = np.equal(label[-10:], label[-10]).all()
+    l1 = np.equal(label[:30], label[0]).all()
+    l2 = np.equal(label[30:-30], label[30]).all()
+    l3 = np.equal(label[-30:], label[-30]).all()
     assert l1
     assert l2
     assert l3
@@ -27,18 +27,18 @@ def test_kmeans():
 
 def test_SC():
     label=spectral_clustering(rand_data, n_clusters=3)
-    l1 = np.equal(label[:10], label[0]).all()
-    l2 = np.equal(label[10:-10], label[10]).all()
-    l3 = np.equal(label[-10:], label[-10]).all()
+    l1 = np.equal(label[:30], label[0]).all()
+    l2 = np.equal(label[30:-30], label[30]).all()
+    l3 = np.equal(label[-30:], label[-30]).all()
     assert l1
     assert l2 
     assert l3
     
 def test_HAC():
     label=HAC(rand_data, n_clusters=3)
-    l1 = np.equal(label[:10], label[0]).all()
-    l2 = np.equal(label[10:-10], label[10]).all()
-    l3 = np.equal(label[-10:], label[-10]).all()
+    l1 = np.equal(label[:30], label[0]).all()
+    l2 = np.equal(label[30:-30], label[30]).all()
+    l3 = np.equal(label[-30:], label[-30]).all()
     assert l1
     assert l2
     assert l3
