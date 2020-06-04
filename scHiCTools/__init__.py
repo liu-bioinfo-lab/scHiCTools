2b013c9b18df598e847a74c0233352bafba3221a
xinjunUM <lixinjun@umich.edu>
2020-06-04 17:45:59 -0400

    Update __init__.py

--- scHiCTools/__init__.py
+++ scHiCTools/__init__.py
@@ -1,4 +1,4 @@
 from .load import scHiCs
 from .analysis import kmeans, spectral_clustering, HAC
-from .analysis import scatter
+from .analysis import scatter, interactive_scatter
 from .embedding import PCA, MDS, tSNE, SpectralEmbedding, PHATE
